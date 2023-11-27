4f3d3502f5909ff66f337f5d1f7851edc7bc5eff
asma <asmaaudah@gmail.com>
2023-11-27 08:39:46 +0700

    add base url

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,13 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://magento.softwaretestingboard.com/',
+    
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    defaultCommandTimeout: 5500,
+    screenshotOnRunFailure: false,
+    chromeWebSecurity: false
   },
 });
--- cypress/e2e/choose-products.cy.js
+++ cypress/e2e/choose-products.cy.js
@@ -1,6 +1,6 @@
 describe('choose product(s)', () => {
     it('From Best Seller and click from the image', () => {
-        cy.visit('https://magento.softwaretestingboard.com/')
+        cy.visit('')
         // Temukan dan pilih produk
         cy.get(':nth-child(1) > .product-item-info > .product-item-photo > .product-image-container > .product-image-wrapper > .product-image-photo').first().click();
         // Verifikasi elemen-elemen yang seharusnya muncul
@@ -21,7 +21,7 @@ describe('choose product(s)', () => {
       })
     it('Pilih dari hasil search', () => {
         // Buka situs web
-        cy.visit('https://magento.softwaretestingboard.com/');
+        cy.visit('');
         // Click search bar
         cy.get('#search').first().click();
         // Ketik keyword dan click enter
@@ -40,7 +40,7 @@ describe('choose product(s)', () => {
         .contains('Compression Short');     
     });
     it.only('From Best Seller and click from the rating', () => {
-        cy.visit('https://magento.softwaretestingboard.com/')
+        cy.visit('')
         // Temukan dan pilih produk
         cy.get(':nth-child(2) > .product-item-info > .product-item-details > .product-reviews-summary > .rating-summary > .rating-result').first().click();
         // Verifikasi bahwa elemen yang seharusnya muncul setelah diklik tidak ada atau tidak terlihat
